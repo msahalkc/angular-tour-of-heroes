ed3ee880c6cf83368aa3f66641f0d1781247c6e3
Muhammed Sahal K C <msahalkc@gmail.com>
2024-06-07 21:51:47 +0530

    build: created an angular app

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NgIf, UpperCasePipe, Location } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css',
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero?: Hero;
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
+  }
+}
